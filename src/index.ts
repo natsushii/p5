e2116130b694d5f3f15e04c9c0e0f73308e6a5d0
natsushii <natalia.correa@iteso.mx>
2026-09-22 21:29:07 -0600

    SQL lite bd

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,18 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+export interface Env {
+	p6: D1Database;
+}
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response("Hello World! New change natibramch");
+		const data = await this.queryDatabase(env.p6);
+		return new Response.json({message: "Hello World! New change natibramch", dbData: data);
 	},
+
+	async queryDatabase(db: D1Database){
+		const { results } = await db.prepare("SELECT * FROM users").all();
+		return results;
+	}
 } satisfies ExportedHandler<Env>;
